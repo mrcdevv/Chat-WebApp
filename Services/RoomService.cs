051fd17ac47b874f5e6b2dbc606ab1b47beb6288
mrc <marcoemorales2015@gmail.com>
2024-04-10 23:51:28 -0300

    Interfaces Created

---
+++ Services/RoomService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tts.Services
+{
+    public class RoomService
+    {
+        
+    }
+}
