51596ef6c8ee24735e35e6e5a4adc3111c18d2a9
mrc <marcoemorales2015@gmail.com>
2024-04-14 23:59:27 -0300

    Context added

---
+++ Context/ChatContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatWebApp.Context
+{
+    public class ChatContext : DbContext
+    {
+
+        public ChatContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<RoomUser> RoomUsers { get; set; }
+
+
+
+    }
+}
